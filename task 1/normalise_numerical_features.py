7957fe4633037cbc6c275c3f474170162a828f67
Kanike Kullachanda <kanikekullachanda@gmail.com>
2025-06-25 10:56:20 +0530

    Update normalise_numerical_features.py

--- task 1/normalise_numerical_features.py
+++ task 1/normalise_numerical_features.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv('/Users/kanike/Documents/ai&ml internship/Elevate-Labs-AI-ML-Internship/task 1/Titanic-Dataset.csv')
+df = pd.read_csv('Titanic-Dataset.csv')
 
 scaler = StandardScaler()
 numerical_cols = ['Age', 'Fare', 'SibSp', 'Parch']
